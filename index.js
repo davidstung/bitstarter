f7f489f1d2bd7de8023102407eefa0cdae894966
Ubuntu <ubuntu@ip-172-31-28-161.us-west-2.compute.internal>
2015-11-21 22:42:26 +0000

    added port 8080

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.get('/', function(request, response) {
     response.send(data.toString('utf8'));
 })
 
+var port = process.env.PORT || 8080;
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
