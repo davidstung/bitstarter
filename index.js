222470929b8abde55cb75579be29c21ad58b0713
Ubuntu <ubuntu@ip-172-31-28-161.us-west-2.compute.internal>
2015-11-21 17:59:34 +0000

    index.js

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+var data =fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-    var data = fs.readFileSync('index.html');
-    response.send(data.toString);
+    
+    response.send(data.toString('utf8'));
 })
 
 app.listen(app.get('port'), function() {
